faf177d921fccd12290feadf2609c888b2695c95
WhiteBorshek <121310443+WhiteBorshek@users.noreply.github.com>
2023-01-18 01:42:48 +0500

    Add files via upload

---
+++ Homework I.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 
+*/
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+int b=num/10%10;
+
+
+    Console.Write(b);
---
+++ Homework II.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+int num = Convert.ToInt32(number);
+if (number.Length > 2)
+{
+    Console.WriteLine($"Третья цифра {number [2]}");
+}
+else
+{
+    Console.WriteLine($"Третьей цифры нет");
+}
